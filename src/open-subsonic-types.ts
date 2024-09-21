e950076a603970ea74817efeaa23e499fe2a73b8
jeffvli <jeffvictorli@gmail.com>
2024-09-20 17:44:52 -0700

    chore: add subsonic user type schema

--- src/open-subsonic-types.ts
+++ src/open-subsonic-types.ts
@@ -42,6 +42,23 @@ export const authenticateParameters = z.object({
     v: z.string(),
 });
 
+export const userSchema = z.object({
+    adminRole: z.boolean(),
+    commentRole: z.boolean(),
+    coverArtRole: z.boolean(),
+    downloadRole: z.boolean(),
+    email: z.string().optional(),
+    folder: z.string().array(),
+    playlistRole: z.boolean(),
+    podcastRole: z.boolean(),
+    scrobblingEnabled: z.boolean(),
+    settingsRole: z.boolean(),
+    shareRole: z.boolean(),
+    streamRole: z.boolean(),
+    uploadRole: z.boolean(),
+    username: z.string(),
+});
+
 export const playlistSchema = z.object({
     changed: z.string(),
     created: z.string(),
