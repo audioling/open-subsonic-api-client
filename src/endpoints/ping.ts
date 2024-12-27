d021b77cf97d9981a86e3a3ae0b7d2f7e1b70477
jeffvli <jeffvictorli@gmail.com>
2024-12-27 04:59:59 -0800

    fix: remove explicit auth params from ping endpoint

--- src/endpoints/ping.ts
+++ src/endpoints/ping.ts
@@ -1,5 +1,4 @@
-import { z } from 'zod';
-import { emptyResponseSchema } from '@/open-subsonic-types.js';
+import { emptyRequestSchema, emptyResponseSchema } from '@/open-subsonic-types.js';
 import { createEndpoint, endpointProperties } from '@/utils.js';
 
 const properties = endpointProperties({
@@ -7,24 +6,14 @@ const properties = endpointProperties({
     summary: 'Ping Subsonic server to keep the connection alive.',
 });
 
-const requestSchema = z.object({
-    c: z.string(),
-    f: z.string(),
-    p: z.string().optional(),
-    s: z.string().optional(),
-    t: z.string().optional(),
-    u: z.string().optional(),
-    v: z.string(),
-});
-
 export const ping = {
     ...createEndpoint.ss('SS.1.16.1', {
-        request: requestSchema,
+        request: emptyRequestSchema,
         response: emptyResponseSchema,
         ...properties,
     }),
     ...createEndpoint.os('OS.1', {
-        request: requestSchema,
+        request: emptyRequestSchema,
         response: emptyResponseSchema,
         ...properties,
     }),
